33effa0eaf2e6f434630c3d2ffa297026c1d6d4a
asangepag <a.sangepag@gmail.com>
2021-03-14 20:29:22 -0400

    Create requestProcessor.py

---
+++ requestProcessor/requestProcessor.py
@@ -0,0 +1,30 @@
+import logging
+import datetime
+
+class RequestProcessor:
+  def __init__(self):
+    pass
+  
+  def processRequest(self, user, addresses):
+    '''
+    @param user: string, original user
+    @param addresses: list of addresses for the user
+    returns address to deposit amount
+    '''
+    pass
+ 
+  def processDeposit(self, user, amount):
+    '''
+    @param user: string, original user
+    @param amount: float ,amount user wishes to deposit
+    returns None
+    
+    '''
+    pass
+ 
+
+def main():
+  pass
+  
+  
+  
