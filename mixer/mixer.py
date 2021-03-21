13348f43d31131e0ee4ecdb4afa139f664793b20
asangepag <a.sangepag@gmail.com>
2021-03-21 18:50:29 -0400

    Update mixer.py

--- mixer/mixer.py
+++ mixer/mixer.py
@@ -1,1 +1,21 @@
 
+
+class Mixer:
+  def __init__(self):
+    self.currentTransactions = self.getCurrentTransactions
+  
+  def onUpdate(self, reactor):
+    reactor.callFromThread(self.processUpdate, reactor)
+    
+    
+  def processUpdate(self, reactor):
+    '''
+    get sourceUser and corr sourceAddress
+    amount /= number of sourceaddress
+    use jobcoin api to send money from house_acccout to source address
+    '''
+ 
+def main():
+  m = Mixer()
+  from twisted import reactor
+  
