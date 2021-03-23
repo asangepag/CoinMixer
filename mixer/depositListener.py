90254725e4b72a2e62319d544034c27f1e423d4c
asangepag <a.sangepag@gmail.com>
2021-03-23 01:40:58 -0400

    Update depositListener.py

--- mixer/depositListener.py
+++ mixer/depositListener.py
@@ -4,24 +4,63 @@ This will listen to the incoming transfer to deposit address
 2. making a call to transactions and checking if it changed from the previous stored state. - for test project purposes
 
 '''
+import logging
+import functools
 
 HOUSE_ACCOUNT = 'houseAccount'
+URL = 'http://jobcoin.gemini.com/starlit-synergy/api/transactions'
 
 class DepositListener:
   def __init__(self):
-    self.currentTransactions = self.getCurrentTransactions
+    self.currentTransactions = functools.partial(self.getCurrentTransactions)
+    reactor.callInThread(self.checkForNewTransactions, reactor)
   
   
-  def transferToHouseAccount(amount):
-    pass
+  def transferToHouseAccount(self, newTransactions, reactor):
+    for txn in newTransactions:
+      requestBody = {
+        'fromAddress' : txn['fromAddress']
+        'toAddress' : HOUSE_ACCOUNT'
+        'amount' " txn['amount']
+      }
+      requests.post(URL, body = requestBody)
   
-  def getCurrentTransactions():
+  def getNewTransactions(self, currTxns, reactor):
+    ##get new txns with tempDest as dest and not processes yet, maintain table of processed tempDest'
+    newtxns =  {
+      'fromAddress' : 'Alice'
+      'toAddress' : 'tempDest1'
+      'amount' :'50'
+    }
+    reactor.callFromThread(self.transferToHouseAccount, newTxns, reactor)
+  
+  def checkForNewTransactions(self, reactor):
+    currTxns = self.getCurrentTransactions()
+    if len(currTxns) > self.currentTransactions:
+      reactor.callFromThread(self.getNewTransactions, currTxns, reactor)
+  
+  def getCurrentTransactions(self, reactor):
     '''
     make call to JobCoin API to get txns
     '''
-    pass
+    
+    resp = requests.get(URL)
+    if resp.status_code == 200:
+      resp = json.loads(resp.transactions)
+    else:
+      logging.exception('Error getting txns')
+    
+  def run(self, reactor):
+    if not self.initialized:
+      return
+    reactor.callAfter(5, self.run, reactor)
+    
+  def start(self, reactor, runReactor=True):
+    if runReactor:
+      self.run()
   
 
   def main():
     from twisted import reactor
-    reactor.run()
+    depositListener = DepositListener()
+    depositListener.start()
