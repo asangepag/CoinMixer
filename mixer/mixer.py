455ff20137f6590eddba3ba1f8ae59f893005587
asangepag <a.sangepag@gmail.com>
2021-03-23 01:07:44 -0400

    Update mixer.py

--- mixer/mixer.py
+++ mixer/mixer.py
@@ -1,29 +1,98 @@
-from twisted.internet.task import LoopingCall
+from  twisted.internet.task import LoopingCall
+import  requests
+import  logging
+from  simplejson import json
+
+URL = 'http://jobcoin.gemini.com/starlit-synergy/api/transactions'
+
 
 class Mixer:
-  def __init__(self):
-    self.currentTransactions = self.getCurrentTransactions
+  def __init__(self, updateFrequency=5):
+    self.updateFrequency = updateFrequency
+    self.initialize()
+    self.userSourceAddressMap = self.getUserSourceAddressMap()
+    self.tempDestToOrigUserMap = self.tempDestToOrigUserMap()
+    self.houseAccountAddress = 'houseAccount'
+    
+  def getUserSourceAddressMap(self):
+    ''' reads from where this info is stored by requestProcessor
+        dummying for test
+    '''
+    return {
+      'Alice' : ['Alice1', 'Alice2', 'Alice3'],
+      'Bob' : ['Bob1', 'Bob2', 'Bob3']
+    }
+  
+  def tempDestToOrigUserMap(self):
+    ''' reads from where this info is stored by requestProcessor
+        dummying for test
+    '''
+    return {
+      'tempDest1' : 'Alice'
+      'tempDest2' : 'Bob'
+    }
+ 
   
   def onUpdate(self, reactor):
     reactor.callFromThread(self.processUpdate, reactor)
     
+  
+  def processUpdate(self, currTxns, reactor):
+    for txn in currTxns:
+      tempDest = txn.fromAddress
+      origUser = self.tempDestToOrigUserMap.get(tempDest, None)
+      if not origUser:
+        logging.exceptions('original User not found'
+      sourceAddresses = self.sourceAddressMap.get(origUser, None)
+      if not sourceAddresses:
+        logging.exception('Original User not found')
+      
+      amount = int(txn.amount)
+      splits = amount/len(sourceAddresses)
+      for sa in sourceAddresses:
+        reqBody = {
+          'fromAddress': self.houseAccountAddress,
+          'toAddress' : sa
+          'amount' : str(splits)
+        }
+        requests.post(URL, body=reqBody)
+      
+       
     
-  def processUpdate(self, reactor):
+  def checkForUpdates(self, reactor):
     '''
     get sourceUser and corr sourceAddress
     amount /= number of sourceaddress
     use jobcoin api to send money from house_acccout to source address
     '''
-    pass
+    currTxns = self.getCurrentTransactions
+    #for txn in currTxns:
+      #if txn['new'] == True:
+        #reactor.callFromThread(self.processUpdate, currTxns, reactor)
+        
+    if len(currTxns) > len(self.currentTransactions):
+      reactor.callFromThread(self.processUpdate, currTxns, reactor)
+   
+  
+  def getCurrentTransactions(self, reactor):
+    resp = requests.get(URL)
+    if resp.status_code == 200:
+      resp = json.loads(resp)
+      self.currentTransactions = resp.transactions
+    else:
+      logging.exception('Failed to get current transactions')
   
   def initialize(self, reactor):
     ''' get current txns'''
-    pass
+    self.getCurrentTransactions(reactor)
+    self.initialized = True
+    
   
   def run(self, reactor):
     if not self.initialized:
       return 
-    reactor.callLater(self.refreshFrequency, functools.partial(self.start, reactor)
+    self.checkUpdates(reactor)
+    reactor.callLater(self.updateFrequency, functools.partial(self.run, reactor)
     
   
   def start(self, reactor, runReactor=True):
