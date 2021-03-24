c3b4b4987987ed09271cee5dba11f8c5d78cfa91
asangepag <a.sangepag@gmail.com>
2021-03-24 17:19:43 -0400

    Update mixer.py

--- tests/mixer/mixer.py
+++ tests/mixer/mixer.py
@@ -1,1 +1,19 @@
+import unittest
+from CoinMixer.mixer.mixer import Mixer
+
+class MixerTest(unittest): 
+
+    def setUp(self):
+        self.tempDestToOrigUserMap = {'tempDest123': 'Alice'}
+        self.userSourceAddressMap = {'Alice' : ['Alice1', 'Alice2', 'Alice3']}
+        self.mixer = Mixer()
+
+    def testGetOriginalSourceAddressAndSplits(self):
+        txn = {
+            'fromAddress' : 'tempDest123',
+            'toAddress' :'houseAccount',
+            'amount' : '50'
+        }
+        self.assertEquals(self.mixer.getOriginalSourceAddressAndSplits, (['Alice1', 'Alice2', 'Alcie3'], 16.66666))
+
 
